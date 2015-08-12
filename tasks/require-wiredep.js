0abb4edc22e239316bdab7b1062055711cc5da3a
Krzysztof Padol <krzysztof.padol@pearson.com>
2015-08-12 13:17:04 +0200

    Added new version with generating files + src is no required any more

--- tasks/require-wiredep.js
+++ tasks/require-wiredep.js
@@ -2,7 +2,7 @@
 
 function requireWiredep(grunt) {
   grunt.registerMultiTask('requireWiredep', 'Inject require config into your source code.', function () {
-    this.requiresConfig(['requireWiredep', this.target, 'src']);
+    // this.requiresConfig(['requireWiredep', this.target, 'src']);
 	// Extend the options object with the entire data object (instead of just .src) for backward compatibility.
     require('require-wiredep')(this.options(this.data));
   });
